6863076bbbef544821727896ffac4137395f760a
Brian Cortes <briancorts166@gmail.com>
2018-06-06 22:34:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -31,7 +31,7 @@ function maybeTrue() {
 var doWhileLoop = array => {
   do {
 
-      var array.length = array.length - 1
+      let array.length = array.length - 1
 
   } while (array.length && maybeTrue())
 
