e15d2b9c78f95f914b3e2cedb5d2c4a037116240
Brian Cortes <briancorts166@gmail.com>
2018-06-06 22:12:18 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,26 @@
+var forLoop = array => {
+
+  for (let i = 0; i < 25; i++) {
+
+    if (i === 1) {
+
+      array = [...array, 'I am 1 strange loop.']
+
+    } else {
+
+      array = [...array, `I am ${i} strange loops.`]
+
+    }
+
+}
+ return array
+
+}
+
+var whileLoop = n => {
+
+  while (n > 0) {
+    console.log(--n)
+  }
+  return 'done'
+}
